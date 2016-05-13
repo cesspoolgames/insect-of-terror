fd43846c6b2e8155cb4703b308f26c2f2cd684c1
Amir Eldor <amir@eize.ninja>
2016-05-13 21:03:37 +0300

    Starting with heores shooting poops

---
+++ Assets/Scripts/BossPoop.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossPoop : MonoBehaviour {
+
+    public float horizontalFactor = 1;  // Use this to reverse the direction of the poop
+    public float speed;  // Horizontal speed
+
+    private Rigidbody2D rigidbody2d;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate() {
+        Vector2 constantVelocity = rigidbody2d.velocity;
+        constantVelocity.x = speed * horizontalFactor;
+        rigidbody2d.velocity = constantVelocity;
+    }
+}
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,21 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using MovementEffects;
 
 public class Hero : MonoBehaviour {
 
     public bool startMoveLate;  // Set to true on one of the Heroes so it starts moving in alternation to the other hero
+    public GameObject poop;  // The poop the hero shoots
 
     private HeroCommon masterScript;
     private Vector2 targetPosition;
     private bool waitForMove = false;
     delegate void Behavior();
     Behavior currentBehavior = delegate () { };
+    private bool flipped = false;
+
+    private IEnumerator<float> fireCoroutineHandle;
 
 	// Use this for initialization
 	void Start () {
-        masterScript = GetComponentInParent<HeroCommon>();
+        masterScript = GameObject.Find("HeroesCommon").GetComponent<HeroCommon>();
         targetPosition = transform.position;
         Invoke("StartMoving", startMoveLate ? masterScript.timeBetweenMoves : 0);
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Debug.Log(spriteRenderer.flipX);
+        if (spriteRenderer.flipX) {
+            flipped = true;  // Mark that we are flipped so we know when to shoot things reversed etc.
+        }
+        StartFiring();
 	}
 
     void StartMoving() {
@@ -36,6 +48,33 @@ public class Hero : MonoBehaviour {
         // Calculate where do we want the hero to go (mouse's Y coordinate)
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         targetPosition.y = mousePosition.y;
+    }
+
+    IEnumerator<float> _FirePoop() {
+        while (true) {
+            if (waitForMove) {
+                yield return 0f;
+            } else {
+                // Create poop
+                Quaternion rotation = Quaternion.identity;
+                Vector3 position = transform.position;
+
+                GameObject newPoop = (GameObject)Instantiate(poop, position, rotation);
+                if (flipped) {
+                    newPoop.GetComponent<BossPoop>().horizontalFactor = -1;
+                }
+
+                yield return Timing.WaitForSeconds(.3f);
+            }
+        }
+    }
+
+    void StartFiring() {
+        fireCoroutineHandle = Timing.RunCoroutine(_FirePoop());
+    }
+
+    void StopFiring() {  // FIXME: Might not be used? Do we have constant fire? If so no need for the coroutine handle variable
+        Timing.KillCoroutine(fireCoroutineHandle);
     }
 
     void FixedUpdate() {
