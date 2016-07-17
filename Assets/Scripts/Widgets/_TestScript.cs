b0fbb613c38177d53f6f2e1a12627595253b9511
Amir Eldor <amir@eize.ninja>
2016-07-17 23:49:47 +0300

    Created a test scene with nice image numbers
    These number might build up a counter widget which will be beautiful.
    I want its numbers to motion-blur nicely when changing.

---
+++ Assets/Scripts/Widgets/_TestScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Sprite[] numbers = Resources.LoadAll<Sprite>("Widgets/roachnumberspng") as Sprite[];
+        Debug.Log(numbers.Length);
+        for (int i = 0; i <= 9; i++) {
+            GameObject newObject = new GameObject();
+            newObject.AddComponent<SpriteRenderer>();
+            SpriteRenderer spriteRenderer = newObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = numbers[i];
+            newObject.transform.position = new Vector3(i - 5, 0, 0);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
