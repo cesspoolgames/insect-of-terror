0d23cc145233c2d55d47ecc934db62c11eeab078
Amir Eldor <amir@eize.ninja>
2016-07-22 21:39:23 +0300

    Oh my glob, I wrote a unit test for the NumberWidget

---
+++ Assets/Editor/NumberWdigetTests.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class NumberWdigetTests {
+
+    [Test]
+    public void EditorTest()
+    {
+        //Arrange
+        var widget1 = new NumberWidget();
+
+        widget1.number = 0;
+        Assert.AreEqual(widget1.NumberOfDigits(), 1);
+
+        widget1.number = 1;
+        Assert.AreEqual(widget1.NumberOfDigits(), 1);
+
+        widget1.number = 10;
+        Assert.AreEqual(widget1.NumberOfDigits(), 2);
+
+        widget1.number = 12;
+        Assert.AreEqual(widget1.NumberOfDigits(), 2);
+
+        widget1.number = 30;
+        Assert.AreEqual(widget1.NumberOfDigits(), 2);
+
+        widget1.number = 100;
+        Assert.AreEqual(widget1.NumberOfDigits(), 3);
+
+        widget1.number = 101231;
+        Assert.AreEqual(widget1.NumberOfDigits(), 6);
+    }
+}
---
+++ Assets/Scripts/Widgets/NumberWidget.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+public class NumberWidget : MonoBehaviour {
+
+    static Sprite[] numberSprites;
+
+    public int number = 0;  // TODO: Handle negative numbers
+
+    protected GameObject[] digits;  // The digits making up the number. Each index is another digit to the left of the number (i.e. 10's 100's 1000's)
+
+    // Use this for initialization
+    void Start() {
+        if (numberSprites == null) {
+            numberSprites = Resources.LoadAll<Sprite>("Widgets/roachnumberspng") as Sprite[];
+            Assert.AreEqual(numberSprites.Length, 10);
+            Debug.Log(numberSprites.Length);
+        }
+    }
+
+    public int NumberOfDigits() {
+        int numberOfDigits = 1;
+        if (number >= 10) {
+            numberOfDigits = Mathf.FloorToInt(Mathf.Log10(number)) + 1;
+        }
+        Debug.Log(numberOfDigits);
+        return numberOfDigits;
+    }
+
+	// Update is called once per frame
+	void Update () {
+    }
+}
